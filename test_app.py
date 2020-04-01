d5c1b89e89033a0e27cdc3fd4d86bbc6cc778c5d
ruchirbhai <ruchirbhai@gmail.com>
2020-04-01 06:05:37 -0700

    trying to use pytst with flask
    Currently not working

--- test_app.py
+++ test_app.py
@@ -1,11 +1,14 @@
+from flask import Flask, jsonify
 import unittest
-import app
+from app import app
 
-class TestApp(unittest.TestCase):
-
-    def test_add(self):
-        result = app.add(100, 20)
-        self.assertEqual(result, 120)
+@pytest.fixture
+def test_add(self):
+    json, rv = self. '/add')
+    self.assertTrue(json == 120)
+    # tester = app.test_client(self)
+    # response = tester.post(jsonify({"number_1": "100","number_2": "20"}))
+    # self.assertEqual(response, 120)
 
 if __name__ == '__main__':
     unittest.main()
