2ae5b64896fa7bed35b116f36d71f13199c71e1a
ruchirbhai <ruchirbhai@gmail.com>
2020-04-02 00:55:32 -0700

    added data validation checks and updated add route

--- app.py
+++ app.py
@@ -1,57 +1,62 @@
 # Import flask libraries
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, jsonify, request
 
 # Create the flask app
 app = Flask(__name__)
 # Logger.error, check for flask error, flask info
 
-# function to sanitize the input
-def inp_sanitize(num1, num2):
-    # Error 2: for an empty input get ValueError: could not convert string to float:
-    # Error 3: check Number_1 and number_2
-    # Error 4: check if its json
-    # Error 5: check for strings
-    # Error 6:
-    print(num1, num2)
-    # if num1 or num2 == '':
-    #     error = jsonify({"error2": "Data empty"})
-    #     print (error)
-    #     return error
+
+# function to validate the input
+def inp_validation(data):
+    app.logger.error("inside Input Validation")
+
+    # Error 1: Check if the keys are correct expected number_1 and number_2
+    if not ("number_1" in data and "number_2" in data):
+        error = "Invalid input: Expected {\"number_1\": \"number\", \"number_2\": \"number\"}"
+        raise TypeError(error)
+
+    # Error 2: Ensure that the values are not empty
+    if data.get("number_1") == '' or data.get("number_2") == '':
+        error = "Invalid input: Expected {\"number_1\": \"number\", \"number_2\": \"number\"}"
+        raise TypeError(error)
+
+    # convert the input data to float, any non-numerical data caught in the general exception
+    num1 = float(data.get("number_1"))
+    num2 = float(data.get("number_2"))
+    app.logger.error(num1, num2)
+
+    # return validated and converted data back
     return num1, num2
-
-
-# Create homepage App route for the calculator
-# Modify the homepage to show success or error
-@app.route('/')
-def hello():
-    return 'hello world'
 
 
 # route for using Json
 @app.route('/add', methods=['POST'])
 def add():
-    # add this to try catch,
     try:
-        if request.is_json:
-            req = request.get_json()
-            num1 = float(req.get("number_1"))
-            num2 = float(req.get("number_2"))
-            print("hello")
-            num1, num2 = inp_sanitize(num1, num2)
-            # improvement remove add and print statements+
-            print(num1, num2)
-            add = num1 + num2
-            print(add)
-            res = jsonify({"result": add})
+        app.logger.error("Calling add")
+        # Request data in Json format, any JSON formatting issues caught by general Exception
+        req = request.get_json()
 
-            return res, 200
-        else:
-            res = jsonify({"error1": "Data is not is JSON format"})
-            return res, 400
-    #raise value error
-    exception as e:
-        error = " This error"
-        return error, 400
+        # Send values for validation
+        num1, num2 = inp_validation(req)
+
+        # perform operations on validated data
+        add = num1 + num2
+        app.logger.error(add)
+
+        return jsonify({"result": add}), 200
+
+    except TypeError as e:
+        app.logger.error(str(e))
+        return jsonify({"error": str(e)}), 400
+
+    except ValueError as e:
+        app.logger.error(str(e))
+        return jsonify({"error": str(e)}), 400
+
+    except Exception as e:
+        app.logger.error(e)
+        return jsonify({"error": str(e)}), 400
 
 
 @app.route('/subtract', methods=['POST'])
